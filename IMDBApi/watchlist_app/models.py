a3c15a53cbfb0b8ef399635e731f96f12ea7ef6a
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-12 21:54:51 +0100

    StreamPlateform model was created

--- IMDBApi/watchlist_app/models.py
+++ IMDBApi/watchlist_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 # Create your models here.
+
+class StreamPlateform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True) #is released or launched
+    created = models.DateTimeField(auto_now_add=True)
+    avg_rating = models.FloatField(default=0)
+    number_rating = models.IntegerField(default=0)
+    
+    plateform = models.ForeignKey(StreamPlateform, on_delete=models.CASCADE, related_name="watchlist")
+    
+    def __str__(self):
+        return self.title
+    
