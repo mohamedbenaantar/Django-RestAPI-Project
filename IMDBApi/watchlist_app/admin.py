a509da2500fc3a7f2ad7bf6aefc9f3ca461672eb
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-12 21:57:42 +0100

    Make Migrations and submit the models to Admin panel

--- IMDBApi/watchlist_app/admin.py
+++ IMDBApi/watchlist_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import WatchList, StreamPlateform, Review
 # Register your models here.
+admin.site.register(WatchList)
+admin.site.register(StreamPlateform)
